8d0d3df1868bdc4422e54d4e2e07b4685daedf18
Matheus Mendes <matheusmendes@Matheuss-MacBook.local>
2025-06-18 23:44:22 +0100

    onclick routes  admin

--- src/components/AdminLayout.tsx
+++ src/components/AdminLayout.tsx
@@ -1,10 +1,13 @@
 import { ReactNode } from 'react';
+import { useRouter } from 'next/router';
 
 interface AdminLayoutProps {
   children: ReactNode;
 }
 
 export default function AdminLayout({ children }: AdminLayoutProps) {
+  const router = useRouter();
+
   return (
     <div className="flex">
       {/* Botão para abrir o menu lateral em telas pequenas */}
@@ -34,7 +37,7 @@ export default function AdminLayout({ children }: AdminLayoutProps) {
                   <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
                   <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
                 </svg>
-                <span className="ml-3">Overview</span>
+                <span className="ml-3" onClick={() => router.push('/admin/dashboard')}>Overview</span>
               </a>
             </li>
             <li>
@@ -42,7 +45,9 @@ export default function AdminLayout({ children }: AdminLayoutProps) {
                 <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-400 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z" clipRule="evenodd"></path>
                 </svg>
-                <span className="flex-1 ml-3 text-left whitespace-nowrap">Upload</span>
+                <span className="flex-1 ml-3 text-left whitespace-nowrap" onClick={() => router.push('/admin/upload')}>
+                  Upload
+                </span>
                 <svg aria-hidden="true" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path>
                 </svg>
@@ -53,7 +58,7 @@ export default function AdminLayout({ children }: AdminLayoutProps) {
                 <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-400 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd"></path>
                 </svg>
-                <span className="flex-1 ml-3 text-left whitespace-nowrap">Relatórios</span>
+                <span className="flex-1 ml-3 text-left whitespace-nowrap" onClick={() => router.push('/admin/report')}>Relatórios</span>
                 <svg aria-hidden="true" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path>
                 </svg>
